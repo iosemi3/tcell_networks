9e42bc4cd5a273e65504f56b89f90d397a1db8c4
iosemi3 <150516535+iosemi3@users.noreply.github.com>
2024-05-28 13:04:04 +0200

    Update step1_create_joint_basic_analysis.R

--- step1_create_joint_basic_analysis.R
+++ step1_create_joint_basic_analysis.R
@@ -1,96 +1,149 @@
-# Load necessary libraries
-library(Seurat)
-library(SingleR)
-library(celldex)
-library(EnsDb.Hsapiens.v86)
 library(ggplot2)
 library(dplyr)
-
-# Set seed for reproducibility
+library(celldex)
+library(Signac)
+library(Seurat)
+library(chromVAR)
+library(motifmatchr)
+library(Matrix)
+library(SummarizedExperiment)
+library(BiocParallel)
+library(hdf5r)
+#library(GenomicRanges)
+library(SingleR)
+library(BSgenome.Hsapiens.UCSC.hg38)
+library(EnsDb.Hsapiens.v86)
+library(JASPAR2020)
+library(TFBSTools)
+#library(rhdf5)
+library(plotly)
 set.seed(1234)
-
-# Define input and output paths
+################-----SNAKEMAKE
 input_feature_matrix <- snakemake@input$input_feature_matrix
 input_fragments <- snakemake@input$input_fragments
+#input_feature_matrix <- Read10X_h5("/Users/sebas/documents/atac_tcell/from_server/filtered_feature_bc_matrix.h5")
+#input_fragments <- "/Users/sebas/documents/atac_tcell/from_server/atac_fragments.tsv.gz"
+
 umap_plot_file <- "results/umap_plot.pdf"
-piechart_plot_file <- 'results/piechart.pdf'
-pmbc_rdata_file <- 'results/pmbc_1103.rds'
+piechart_plot_file<- 'results/piechart.pdf'
+pmbc_rdata_file<- 'results/pmbc_1103.rds'
 
-# Load the RNA and ATAC data
+
+################
+################
+################-----BLOCK 1: DATA PROCESSING 
+################
+################
+
+#-=-=-=-=-=-=-=-=-=
+# load the RNA and ATAC data
 counts <- Read10X_h5(input_feature_matrix)
-fragpath <- input_fragments
-
-# Get gene annotations for hg38
-annotation <- GetGRangesFromEnsDb(ensdb = EnsDb.Hsapiens.v86)
+fragpath <- paste(input_fragments)
+# get gene annotations for hg38
+annotation <-GetGRangesFromEnsDb(ensdb = EnsDb.Hsapiens.v86)
 seqlevelsStyle(annotation) <- "UCSC"
 
-# Create a Seurat object containing the RNA data
-pbmc <- CreateSeuratObject(counts = counts$`Gene Expression`, assay = "RNA")
+# create a Seurat object containing the RNA adata
+pbmc <- CreateSeuratObject(
+  counts = counts$`Gene Expression`,
+  assay = "RNA"
+)
+# create ATAC assay and add it to the object
+pbmc[["ATAC"]] <- CreateChromatinAssay(
+  counts = counts$Peaks,
+  sep = c(":", "-"),
+  fragments = fragpath,
+  annotation = annotation
+)
+pmbc_1103 <-pbmc
+pmbc_1103
+subset_cells <- sample(colnames(pmbc_1103), 1500)
+pmbc_1103 <- subset(pmbc_1103, cells = subset_cells)
 
-# Create ATAC assay and add it to the object
-pbmc[["ATAC"]] <- CreateChromatinAssay(counts = counts$Peaks, sep = c(":", "-"), fragments = fragpath, annotation = annotation)
+remove(counts)
+remove(annotation)
 
-# Subset to 1000 cells for faster processing
-subset_cells <- sample(colnames(pbmc), 1000)
-pbmc <- subset(pbmc, cells = subset_cells)
+############---RNA-------#############
 
-# RNA data processing
-pbmc[["percent.mt"]] <- PercentageFeatureSet(pbmc, pattern = "^MT")
-pbmc[['percent.ribo']] <- PercentageFeatureSet(pbmc, pattern = "^RP[SL]")
+pmbc_1103[["percent.mt"]] <- PercentageFeatureSet(pmbc_1103, pattern = "^MT")
+pmbc_1103[['percent.ribo']]<- PercentageFeatureSet(pmbc_1103, "^RP[SL]")
 
-# Filtering cells
-pbmc <- subset(pbmc, subset = nFeature_RNA > 200 & nFeature_RNA < 7000 & percent.mt < 7.5 & nCount_RNA > 500 & nCount_RNA < 11000)
+###-filtering by nr of features and prct mt-###
+pmbc_1103<- subset(pmbc_1103, 
+                   subset = nFeature_RNA > 200 & nFeature_RNA < 7000 & percent.mt < 7.5 &nCount_RNA > 500 &nCount_RNA<11000)
 
-# Normalize data and perform cell cycle scoring
-pbmc <- NormalizeData(pbmc, normalization.method = 'LogNormalize')
+pmbc_1103<- NormalizeData(pmbc_1103,normalization.method = 'LogNormalize')
+#cell cycle scoring
 s.genes <- cc.genes.updated.2019$s.genes
 g2m.genes <- cc.genes.updated.2019$g2m.genes
-pbmc <- CellCycleScoring(pbmc, s.features = s.genes, g2m.features = g2m.genes, set.ident = TRUE)
-pbmc <- ScaleData(pbmc, vars.to.regress = c('S.Score', 'G2M.Score'))
+pmbc_1103<- CellCycleScoring(pmbc_1103, s.features = s.genes, g2m.features = g2m.genes, set.ident = TRUE)
+pmbc_1103<- ScaleData(pmbc_1103, vars.to.regress = c('S.Score','G2M.Score'))
 
-# Find highly variable features and perform PCA
-pbmc <- FindVariableFeatures(pbmc, selection.method = 'mean.var.plot', verbose = TRUE)
-pbmc <- RunPCA(pbmc, npcs = 30, verbose = TRUE)
+###-HIGHLY VARIABLE FEATURE ANALYSIS-###
 
-# Find neighbors and clusters
-pbmc <- FindNeighbors(pbmc, dims = 1:30)
-pbmc <- FindClusters(pbmc, resolution = 0.2)
+pmbc_1103<-FindVariableFeatures(pmbc_1103, selection.method = 'mean.var.plot',binning.method = 'equal_frequency', verbose = T)
+#pmbc_1103<- ScaleData(pmbc_1103, features = rownames(pmbc_1103))
 
-# Automatic cell annotation by SingleR
-dice <- celldex::DatabaseImmuneCellExpressionData()
-pred.dice <- SingleR(test = as.SingleCellExperiment(pbmc), ref = dice, assay.type.test = 1, labels = dice$label.fine)
-pbmc@meta.data <- cbind(pbmc@meta.data, pred.dice)
-colnames(pbmc@meta.data)[which(names(pbmc@meta.data) == "labels")] <- "labels.DICE"
-Idents(pbmc) <- pbmc@meta.data$labels.DICE
+#####
+pmbc_1103<- RunPCA(pmbc_1103,ndims.print = c(1,3,5), verbose = T)
+pmbc_1103<- FindNeighbors(pmbc_1103, dims = 1:30)
+pmbc_1103<- FindClusters(pmbc_1103, resolution = 0.2)
+# marker genes
+cluster_mar_gene=FindAllMarkers(pmbc_1103, only.pos = F, test.use = 'negbinom')
 
-# Dropping unwanted cells
-unwanted_cells <- c('Monocytes, CD16+', 'B cells, naive', 'T cells, CD4+, TFH', 'T cells, CD8+, naive', 'T cells, CD8+, naive, stimulated')
-pbmc <- subset(pbmc, idents = unwanted_cells, invert = TRUE)
-
-# Run UMAP
-pbmc <- RunUMAP(pbmc, dims = 1:30, reduction = 'pca', reduction.name = 'umap')
-
-# Export UMAP plot
-umap_plot <- DimPlot(pbmc, reduction = "umap", pt.size = 1.5)
+###cell anotation
+### automatic cell annotation by SingleR
+##-PREP
+#####
+dice<-celldex::DatabaseImmuneCellExpressionData()
+#####
+##Dice
+#####
+pred.dice <- SingleR(test = as.SingleCellExperiment(pmbc_1103), ref = dice, assay.type.test=1,
+                      labels = dice$label.fine)
+##### integrating
+pmbc_1103@meta.data<-cbind(pmbc_1103@meta.data,pred.dice)
+colnames(pmbc_1103@meta.data)[which(names(pmbc_1103@meta.data) == "labels")] <- "labels.DICE"
+Idents(pmbc_1103)=pmbc_1103@meta.data$labels.DICE
+pmbc_1103<- RunUMAP(pmbc_1103, dims = 1:30,reduction = 'PCA_RNA',
+                    reduction.name ='umap' )
+#export as pdf
+umap_plot<-DimPlot(pmbc_1103, reduction = "umap",pt.size = 1.5)
 pdf(umap_plot_file)
 print(umap_plot)
 dev.off()
 
-# Create and export piechart
-piechart <- pbmc@meta.data %>%
-  group_by(seurat_clusters, labels.DICE) %>%
-  summarise(count = n()) %>%
-  mutate(percent = count / sum(count) * 100) %>%
-  ggplot(aes(x = "", y = percent, fill = labels.DICE)) +
-  geom_col(width = 1) +
-  coord_polar("y", start = 0) +
-  facet_wrap(~ seurat_clusters) +
-  theme(axis.text.x = element_blank()) +
-  labs(title = "Celltype abundance per cluster RNA")
+#piechart
+piechart <- tibble(
+  cluster = pmbc_1103$seurat_clusters,
+  cell_type = pmbc_1103$labels.DICE
+) %>%
+  group_by(cluster,cell_type) %>%
+  count() %>%
+  group_by(cluster) %>%
+  mutate(
+    percent=(100*n)/sum(n)
+  ) %>%
+  ungroup() %>%
+  mutate(
+    cluster=paste("Cluster",cluster)
+  ) %>%
+  ggplot(aes(x="",y=percent, fill=cell_type)) +
+  geom_col(width=1) +scale_color_manual(values = c( "T cells, CD4+, naive, stimulated"="darkgreen",
+                                                    "T cells, CD4+, naive"="red",
+                                                    "T cells, CD4+, naive TREG"="black",
+                                                    "T cells, CD4+, Th1"="yellow4",
+                                                    "T cells, CD4+, Th1_17"="royalblue1",
+                                                    "T cells, CD4+, Th2"="darkmagenta")) +
+  coord_polar("y", start=0) +
+  facet_wrap(vars(cluster)) +  
+  theme(axis.text.x=element_blank()) +
+  xlab(NULL) +
+  ylab(NULL)+ggtitle('Celltype abundance per cluster RNA')
 
-pdf(piechart_plot_file)
+pdf(piechart)
 print(piechart)
 dev.off()
 
-# Save the Seurat object
-saveRDS(pbmc, pmbc_rdata_file)
+#export pmbc_1103
+saveRDS(pmbc_1103, pmbc_rdata_file)
